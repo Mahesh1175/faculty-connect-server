c3da71f64f49840219a56eb66f58501bff91054d
Mahesh1175 <mahesh.s.galange@gmail.com>
2026-02-13 19:36:36 +0530

    Used resend mail service instead of simple smtp gmail!

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,32 +1,26 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 console.log("EMAIL:", process.env.EMAIL_USER);
 console.log("PASS exists:", !!process.env.EMAIL_PASS);
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // MUST be false
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 20000, // helps cloud
-});
-
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (to, subject, html, attachments = []) => {
-  await transporter.sendMail({
-    from: `"Campus Visitor System" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-    attachments
-  });
+  try {
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      html,
+      attachments
+    });
+
+    console.log("✅ Email sent:", to);
+
+  } catch (err) {
+    console.log("❌ Email failed:", err.message);
+  }
 };
