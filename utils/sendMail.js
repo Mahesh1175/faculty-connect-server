6f578b66c8d0ac996376a82b2b0055fb55203e5f
Mahesh1175 <mahesh.s.galange@gmail.com>
2026-02-13 19:08:22 +0530

    Fixed the issue for the mail!

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -6,13 +6,14 @@ console.log("EMAIL:", process.env.EMAIL_USER);
 console.log("PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",  
+  port: 587,               
+  secure: false,          
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // app password
+    pass: process.env.EMAIL_PASS,
   },
 });
-
 
 
 export const sendMail = async (to, subject, html, attachments = []) => {
